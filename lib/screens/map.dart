13964df925d211488ae1b1e2dbd51268acf5fbae
shruti <shrutigupta5555@gmail.com>
2021-05-15 16:21:36 +0530

    ADD: Get user's location

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -2,9 +2,8 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:geolocator/geolocator.dart';
 
 class Map extends StatefulWidget {
   const Map({Key key}) : super(key: key);
@@ -17,16 +16,55 @@ class Map extends StatefulWidget {
 
 class _MapState extends State<Map> {
 
+  Completer<GoogleMapController> _controller = Completer();
+
   GoogleMapController mapController;
+  Position _position;
+  String currentLocation;
 
-  void _onMapCreated(GoogleMapController controller)  {
+  void _onMapCreated(GoogleMapController controller) async{
     mapController = controller;
-
   }
 
-    @override
+  @override
   void initState() {
     super.initState();
+    _getCurrentLocation();
+  }
+
+  void _getCurrentLocation() async {
+    LocationPermission permission;
+
+    permission = await Geolocator.checkPermission();
+
+    if(permission == LocationPermission.denied){
+      permission = await Geolocator.requestPermission();
+
+      if(permission == LocationPermission.denied){
+        setState(() {
+          currentLocation = "permission denied";
+        });
+      }else {
+        var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+        setState(() {
+          // currentLocation ="latitude: ${position.latitude}" + " , " + "Logitude: ${position.longitude}";
+          _position = position;
+        });
+      }
+    }else{
+      var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      setState(() {
+        _position = position;
+        // currentLocation ="latitude: ${position.latitude}" + " , " + "Logitude: ${position.longitude}";
+      });
+    }
+    print(_position);
+    if(_position != null){
+
+      CameraUpdate cameraUpdate = CameraUpdate.newLatLngZoom(LatLng(_position.latitude, _position.longitude), 15);
+      mapController.animateCamera(cameraUpdate);
+    }
+
   }
 
 
@@ -38,13 +76,17 @@ class _MapState extends State<Map> {
         ),
         body: GoogleMap(
           onMapCreated: _onMapCreated,
-
+          myLocationButtonEnabled: true,
           initialCameraPosition: CameraPosition(
             target: LatLng(37.43296265331129, -122.08832357078792),
             zoom: 11.0,
           ),
 
         ),
+
+
+
+
 
 
 
