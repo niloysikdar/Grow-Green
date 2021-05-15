f40477de1c6fea3b1f172073b0c6dbf43cec5a30
shruti <shrutigupta5555@gmail.com>
2021-05-15 17:35:59 +0530

    ADD: Dummy marker and bottom nav bar

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -22,7 +22,12 @@ class _MapState extends State<Map> {
   Position _position;
   String currentLocation;
 
-  void _onMapCreated(GoogleMapController controller) async{
+  //adding markers
+  List<Marker> myMarker = <Marker>[];
+
+  int _selectedIndex = 0;
+
+  void _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
   }
 
@@ -30,67 +35,96 @@ class _MapState extends State<Map> {
   void initState() {
     super.initState();
     _getCurrentLocation();
+
+    myMarker.add(
+        Marker(
+          markerId: MarkerId("1"),
+          position: LatLng(19.2481883, 73.1539467),
+        )
+    );
   }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
 
   void _getCurrentLocation() async {
     LocationPermission permission;
 
     permission = await Geolocator.checkPermission();
 
-    if(permission == LocationPermission.denied){
+    if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
 
-      if(permission == LocationPermission.denied){
+      if (permission == LocationPermission.denied) {
         setState(() {
           currentLocation = "permission denied";
         });
-      }else {
-        var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      } else {
+        var position = await Geolocator.getCurrentPosition(
+            desiredAccuracy: LocationAccuracy.high);
         setState(() {
           // currentLocation ="latitude: ${position.latitude}" + " , " + "Logitude: ${position.longitude}";
           _position = position;
         });
       }
-    }else{
-      var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    } else {
+      var position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
       setState(() {
         _position = position;
         // currentLocation ="latitude: ${position.latitude}" + " , " + "Logitude: ${position.longitude}";
       });
     }
     print(_position);
-    if(_position != null){
-
-      CameraUpdate cameraUpdate = CameraUpdate.newLatLngZoom(LatLng(_position.latitude, _position.longitude), 15);
+    if (_position != null) {
+      CameraUpdate cameraUpdate = CameraUpdate.newLatLngZoom(
+          LatLng(_position.latitude, _position.longitude), 15);
       mapController.animateCamera(cameraUpdate);
     }
-
   }
 
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("App"),
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App"),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        myLocationButtonEnabled: true,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(37.43296265331129, -122.08832357078792),
+          zoom: 11.0,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          myLocationButtonEnabled: true,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(37.43296265331129, -122.08832357078792),
-            zoom: 11.0,
+        markers: Set.from(myMarker),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "home"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "fav"
+
           ),
 
-        ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: _onItemTapped,
+      ),
 
 
-
-
-
-
-
-      );
-    }
+    );
   }
+
+}
 
